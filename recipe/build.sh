64c0ee9a89a0c26a06d6022fd1c860fafb8b8d4e
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2020-07-02 17:58:48 -0700

    Turn off debug flags

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,8 +5,8 @@ mkdir build && cd build
 # Make libraries
 cmake                                          \
   -DCMAKE_BUILD_TYPE=Release                   \
-  -DCMAKE_CXX_FLAGS='-g -O3 -std=c++11'        \
-  -DCMAKE_C_FLAGS='-g -O3'                     \
+  -DCMAKE_CXX_FLAGS='-O3 -std=c++11'           \
+  -DCMAKE_C_FLAGS='-O3'                        \
   -DCMAKE_INSTALL_PREFIX:PATH=${PREFIX}        \
   -DEIGEN_INCLUDE_DIR=${PREFIX}/include/eigen3 \
   -DCMAKE_PREFIX_PATH=${PREFIX}                \
