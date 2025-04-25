944eb3c4285aceb3ba8e0c5f717228f5daee0067
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2025-04-25 09:54:45 -0700

    Sync up with latest in libpointmatcher

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,21 +5,21 @@ mkdir build && cd build
 # Make libraries
 cmake                                          \
   -DCMAKE_BUILD_TYPE=Release                   \
-  -DCMAKE_CXX_FLAGS='-O3 -std=c++17'           \
+  -DCMAKE_CXX_COMPILER_ARCHITECTURE_ID=x64     \
+  -DCMAKE_CXX_FLAGS="-O3 -std=c++17"           \
   -DCMAKE_C_FLAGS='-O3'                        \
-  -DCMAKE_INSTALL_PREFIX:PATH=${PREFIX}        \
-  -DEIGEN_INCLUDE_DIR=${PREFIX}/include/eigen3 \
+  -DCMAKE_INSTALL_PREFIX=${PREFIX}             \
+  -DCMAKE_VERBOSE_MAKEFILE=ON                  \
   -DCMAKE_PREFIX_PATH=${PREFIX}                \
-  -DBoost_DIR=${PREFIX}/lib                    \
-  -DBoost_INCLUDE_DIR:PATH=${PREFIX}/include   \
-  -DLIBNABO_INSTALL_DIR=${PREFIX}              \
+  -DCMAKE_VERBOSE_MAKEFILE=ON                  \
+  -DUSE_SYSTEM_YAML_CPP=OFF                    \
   -DBUILD_SHARED_LIBS=ON                       \
-  -DCMAKE_VERBOSE_MAKEFILE=ON                  \
+  -DEIGEN_INCLUDE_DIR=${PREFIX}/include/eigen3 \
+  -DBoost_DIR=${PREFIX}/lib                    \
+  -DBoost_INCLUDE_DIR=${PREFIX}/include        \
   -DBoost_NO_BOOST_CMAKE=OFF                   \
-  -DCMAKE_VERBOSE_MAKEFILE=ON                  \
   -DBoost_DEBUG=ON                             \
   -DBoost_DETAILED_FAILURE_MSG=ON              \
-  -DCMAKE_CXX_COMPILER_ARCHITECTURE_ID=x64     \
   -DBoost_NO_SYSTEM_PATHS=ON                   \
   ${SRC_DIR}
 
